e6f6798b4a2f432ef26519c41dfa3fe3eb6e6a00
Bill <webragg@icloud.com>
2019-01-14 09:37:41 -0600

    Update Joke.js

--- Joke.js
+++ Joke.js
@@ -3,7 +3,7 @@ import React from "react"
 function Joke(props) {
     return (
         <div>
-            <h3>{props.question}</h3>
+            <h3 style={{display: !props.question && "none"}}>{props.question}</h3>
             <h3>Answer: {props.answer}</h3>
             <hr />
         </div>
